05378a0b0b5def599414079c5a3ab7d13d7be852
misseven0 <misseven@protonmail.com>
2023-12-17 17:10:00 +0800

    update mod2

--- example_test.go
+++ example_test.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/jonboulle/clockwork"
-	. "github.com/missevne0/gocron/v2" // nolint:revive
+	. "github.com/misseven0/gocron/v2" // nolint:revive
 )
 
 func ExampleAfterJobRuns() {
